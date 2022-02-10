f5d0d8f19f3242924b573edd80c80e18e75adbab
3061148499 <3061148499@qq.com>
2022-02-10 10:40:56 +0800

    role.vue

---
+++ src/api/copySample.js
@@ -0,0 +1,30 @@
+// todo 模拟 https://el-admin.xin/system/user
+import request from '@/utils/request';
+// const baseUrl = 'https://www.fastmock.site/mock/c25e396c1d2ef07e8f1fc0acf0adc1a5/vue-helper';
+// const baseUrl1 = 'https://elm.cangdu.org';
+const baseUrl = 'https://el-admin.xin/api';
+
+// export function roleTableAll() {
+//   return request({
+//     url: baseUrl1 + '/v1/users/list',
+//     method: 'get'
+//   })
+// }
+
+
+// export function DataAll(a, b) {
+//   return request({
+//     url: baseUrl1 + '/v1/users/list?offset=' + a + '&limit=' + b,
+//     method: 'get'
+//   })
+// }
+
+
+// export function getRoleData() {
+//   return request({
+//     url: baseUrl + '/role-data',
+//     method: 'get'
+//   })
+// }
+
+
--- src/views/system/role/role.vue
+++ src/views/system/role/role.vue
@@ -1,40 +1,141 @@
 <template>
-  <div>
-    <span> 角色管理 </span>
+  <div style="width: 100%;height: 100%;background-color: #fff">
+    <el-row>
+      <el-col :span="6">
+        <el-input
+          style="width: 60%"
+          size="small"
+          v-model="filterText"
+          placeholder="输入部门名称搜索"></el-input>
+        <p></p>
+        <el-tree
+          class="filter-tree"
+          :data="data"
+          :props="defaultProps"
+          default-expand-all
+          :filter-node-method="filterNode"
+          ref="tree">
+        </el-tree>
+      </el-col>
+
+      <el-col :span="18" style="border: 1px solid blue">
+        <div class="search-container">
+          <el-row>
+            <el-col class="public-border" :span="7">
+              <el-input size="small" v-model="name" placeholder="输入名称或邮箱搜索"></el-input>
+            </el-col>
+            <el-col class="public-border" :span="7">
+              <date-range-picker v-model="createTime" class="date-item" />
+            </el-col>
+            <el-col class="public-border" :span="4">10</el-col>
+            <el-col class="public-border" :span="3">10</el-col>
+            <el-col class="public-border" :span="3">10</el-col>
+          </el-row>
+        </div>
+        <div class="button-group"></div>
+        <div class="table-content"></div>
+      </el-col>
+    </el-row>
   </div>
 </template>
 
 <script>
-  import {roleTableAll, DataAll, getRoleData} from "../../../api/system";
-
+  import {roleTableAll, DataAll, getRoleData} from '../../../api/system'
+  import dateRangePicker from '../../../components/DateRangePicker/dateRangePicker'
   export default {
-    name: "role",
-    components: {},
+    name: 'role',
+    components: {
+      dateRangePicker,
+    },
     props: {},
     computed: {},
     filters: {},
-    watch: {},
-    data() {
-      return {}
+    watch: {
+      filterText (val) {
+        this.$refs.tree.filter(val)
+      }
     },
-    methods: {},
-    created() {
-      DataAll(
-        10, 88
-      )
-        .then((res) => {
+    data () {
+      return {
+        filterText: '', // 部门名称
+        data: [
+          {
+            id: 1,
+            label: '一级 1',
+            children: [{
+              id: 4,
+              label: '二级 1-1',
+              children: [{
+                id: 9,
+                label: '三级 1-1-1'
+              }, {
+                id: 10,
+                label: '三级 1-1-2'
+              }]
+            }]
+          }, {
+            id: 2,
+            label: '一级 2',
+            children: [{
+              id: 5,
+              label: '二级 2-1'
+            }, {
+              id: 6,
+              label: '二级 2-2'
+            }]
+          }, {
+            id: 3,
+            label: '一级 3',
+            children: [{
+              id: 7,
+              label: '二级 3-1'
+            }, {
+              id: 8,
+              label: '二级 3-2'
+            }]
+          }],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        },
+        name: '', // 名称或邮箱
+        createTime: '',
+      }
+    },
+    methods: {
+      filterNode (value, data) {
+        if (!value) return true
+        return data.label.indexOf(value) !== -1
+      },
+      dataAll () {
+        DataAll(
+          10, 88
+        )
+          .then((res) => {
+            console.log(res)
+          })
+      },
+      getRole () {
+        getRoleData().then((res) => {
           console.log(res)
-        });
-
-      getRoleData().then((res) => {
-        console.log(res)
-      })
+        })
+      },
     },
-    mounted() {
+    created () {
+      // this.dataAll()
+      // this.getRole()
+    },
+    mounted () {
     },
   }
 </script>
 
 <style lang="scss" scoped>
-
+  /*.date-item {*/
+  /*  display: inline-block;*/
+  /*  vertical-align: middle;*/
+  /*  margin-bottom: 10px;*/
+  /*  height: 30.5px !important;*/
+  /*  width: 230px !important;*/
+  /*}*/
 </style>
